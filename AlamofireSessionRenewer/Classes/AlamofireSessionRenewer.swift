eb7e3b04fca85e43b524e59579fe66b113b9f994
Kirill Ushkov <kirill.u@itomy.ch>
2019-11-29 16:29:57 +0200

    added documentation

--- AlamofireSessionRenewer/Classes/AlamofireSessionRenewer.swift
+++ AlamofireSessionRenewer/Classes/AlamofireSessionRenewer.swift
@@ -10,13 +10,25 @@ import Alamofire
 public typealias SuccessRenewHandler = (String) -> Void
 public typealias FailureRenewHandler = () -> Void
 
+/// This class is responsible for authentication credentials renewing process
 open class AlamofireSessionRenewer: RequestRetrier {
+    
+    /// Error code indicating request is missing authentication info. Defaults to HTTP code 401
     public let authenticationErrorCode: Int
+    
+    /// Authentication information request header field name
     public let credentialHeaderField: String
+    
+    /// Number of times to retry credentials renewing process upper limit
     public let maxRetryCount: UInt?
     
+    /// Authentication information unit
     open var credential: String?
+    
+    /// Queue which stores requests to retry
     open var queue = SafeQueue()
+    
+    /// Closure which is called when authentication credentials renewing process finishes
     open var renewCredential: ((@escaping SuccessRenewHandler, @escaping FailureRenewHandler) -> Void)?
     
     private lazy var successRenewHandler: SuccessRenewHandler = { [weak self] credential in
@@ -29,11 +41,29 @@ open class AlamofireSessionRenewer: RequestRetrier {
         self?.queue.fullfill(with: false)
     }
     
+    private func addToQueue(requestRetryCompletion: @escaping RequestRetryCompletion) {
+        if queue.add(requestRetryCompletion: requestRetryCompletion) == 1 {
+            renewCredential?(successRenewHandler, failureRenewHandler)
+        }
+    }
+    
     public init(authenticationErrorCode: Int = 401, credentialHeaderField: String = "Authorization", maxRetryCount: UInt? = nil) {
         self.authenticationErrorCode = authenticationErrorCode
         self.credentialHeaderField = credentialHeaderField
         self.maxRetryCount = maxRetryCount
     }
+        
+    /// Method checks whether request contains authentication credentials or not
+    /// - Parameter request: request to check credentials containment
+    open func isCredentialEqual(to request: Request) -> Bool {
+        if let currentCred = credential, let receivedCred = request.task?.originalRequest?.value(forHTTPHeaderField: credentialHeaderField) {
+            return currentCred == receivedCred
+        } else {
+            return false
+        }
+    }
+    
+    // MARK: - RequestRetrier protocol implementation
     
     public func should(_ manager: SessionManager, retry request: Request, with error: Error, completion: @escaping RequestRetryCompletion) {
         if (error as NSError).code == authenticationErrorCode {
@@ -49,20 +79,6 @@ open class AlamofireSessionRenewer: RequestRetrier {
             }
         } else {
             completion(false, 0)
-        }
-    }
-    
-    private func addToQueue(requestRetryCompletion: @escaping RequestRetryCompletion) {
-        if queue.add(requestRetryCompletion: requestRetryCompletion) == 1 {
-            renewCredential?(successRenewHandler, failureRenewHandler)
-        }
-    }
-    
-    open func isCredentialEqual(to request: Request) -> Bool {
-        if let currentCred = credential, let receivedCred = request.task?.originalRequest?.value(forHTTPHeaderField: credentialHeaderField) {
-            return currentCred == receivedCred
-        } else {
-            return false
         }
     }
 }
