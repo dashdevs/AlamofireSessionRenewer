7814ab7434918a1bc1413e64ecac4d3dfab44eaa
Anatolij Klimenko <anatolij.klimenko@hedgehoglab.com>
2021-08-03 17:53:48 +0300

    Added tvOS and watchOS compatibility.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireSessionRenewer",
-    platforms: [.macOS(.v10_12), .iOS(.v10)],
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
     products: [
         .library(name: "AlamofireSessionRenewer", targets: ["AlamofireSessionRenewer"]),
     ],
